a0baa7071040208c9c5249830eeb6e038f1d7ee5
vsilich <disk-3ve3da@yandex.ru>
2019-07-30 21:24:09 +0300

    3

--- src/FtpSender.java
+++ src/FtpSender.java
@@ -1,9 +1,43 @@
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.URL;
+import java.net.URLConnection;
+
+
 public class FtpSender {
-
-
 
     public static void main(String[] args) {
 
+        String ftpUrl = "/";
+        String host = "ftp-srv63801.ht-systems.ru";
+        String user = "srv63801_dash";
+        String pass = "Jg7p73H5";
+        String filePath = "E:\\vsilich\\JAVA\\screens\\4.png";
+        String uploadPath = "\\4.png";
+
+        ftpUrl = String.format(ftpUrl, user, pass, host, uploadPath);
+        System.out.println("Upload URL: " + ftpUrl);
+
+        try {
+            URL url = new URL(ftpUrl);
+            URLConnection conn = url.openConnection();
+            OutputStream outputStream = conn.getOutputStream();
+            FileInputStream inputStream = new FileInputStream(filePath);
+
+            byte[] buffer = new byte[BUFFER_SIZE];
+            int bytesRead = -1;
+            while ((bytesRead = inputStream.read(buffer)) != -1) {
+                outputStream.write(buffer, 0, bytesRead);
+            }
+
+            inputStream.close();
+            outputStream.close();
+
+            System.out.println("File uploaded");
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
 
     }
 }
