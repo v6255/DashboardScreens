ad77aaf23d47ee7d5ebcbb5bf6d9d8c93130743e
vsilich <disk-3ve3da@yandex.ru>
2019-08-03 18:25:14 +0300

    метод SendFile

--- src/FtpSender.java
+++ src/FtpSender.java
@@ -43,8 +43,6 @@ public class FtpSender {
 
     }
 
-    public static void main(String[] args){
-        SendFile("ftp-srv63801.ht-systems.ru", "srv63801_dash", "Jg7p73H5", "E:\\vsilich\\JAVA\\screens\\4.png", "4.png");
-    }
+
 
 }
---
+++ src/FtpToTlgrm.java
@@ -0,0 +1,6 @@
+public class FtpToTlgrm {
+    public static void main(String[] args){
+        FtpSender.SendFile("ftp-srv63801.ht-systems.ru", "srv63801_dash", "Jg7p73H5", "E:\\vsilich\\JAVA\\screens\\4.png", "5.png");
+    }
+}
+
